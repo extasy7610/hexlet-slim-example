fa6f646c4c9096901cb55520eea433dfd9116bd1
Fedya Lavrentyev <lavr@x-cart.com>
2019-08-04 22:28:11 +0400

    Add index page

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+// Подключение автозагрузки через composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Вывод ошибок на экран (для удобной отладки)
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+
+$app = new \Slim\App($configuration);
+
+$app->get('/', function ($request, $response) {
+    return $response->write('Welcome to Slim!');
+});
+$app->run();
